ea8488677f48c3b283bef9f42e8152cde8aca53f
snnmst <sinanmustu.sm@gmail.com>
2019-07-14 01:10:12 +0300

    Django Blog, last commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'SinanM',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'July 14, 2019'
+    },
+    {
+        'author' : 'Jane Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'July 19, 2019'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home </h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About </h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
