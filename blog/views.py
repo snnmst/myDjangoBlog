2f8c92e6f190cfa7eba5342125adf243c41e6aa2
snnmst <sinanmustu.sm@gmail.com>
2019-07-14 22:49:56 +0300

    My Django app, commit 7

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'SinanM',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'July 14, 2019'
-    },
-    {
-        'author' : 'Jane Doe',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'July 19, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
